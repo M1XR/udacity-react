a93d81152a145fa549f61e06100f17e6e5c5c2a4
Mike Rossa <97miro@gmail.com>
2022-03-28 17:48:55 +0200

    create ListContacts with .map

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class ListContacts extends Component {
   render() {
     return (
       <ol className='contact-list'>
         {this.props.contacts.map((contact) => (
-          <li key={contact.id} className='contact-list-item'>
-            <div 
-              className='contact-avatar'
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}
-            ></div>
+          <li key={contact.id}>
+            {contact.name}
           </li>
         ))}
       </ol>
